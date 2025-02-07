a56de01a0ae5ace5199768788083e2b7fa3c5677
monxy747 <12smnms20@gmail.com>
2025-02-07 21:25:51 +0900

    ディレクトリを作成、page.tsxni Xのリンクを追加

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function IndexPage() {
+    return (
+        <>
+            <section>
+                <div>
+                    <Link href="https://x.com/SI_Monxy">Xをフォローする</Link>
+                </div>
+            </section>
+        </>
+    )
+}
