4f0caa4058d4d9e29ae1cca1a150cfc3cf2a838d
monxy747 <12smnms20@gmail.com>
2025-02-07 21:32:27 +0900

    cssでデザインを修正

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 export default function IndexPage() {
     return (
         <>
-            <section>
-                <div>
+            <section className="pt-6 md:pt-10 lg:py-32 pb-8">
+                <div className="container text-center flex flex-col items-center gap-4 max-w-[64rem]">
                     <Link href="https://x.com/SI_Monxy">Xをフォローする</Link>
+                    <h1>Shimon's Tech Blog</h1>
+                    <p>ソフトウェアエンジニア 史門のテックブログです。</p>
                 </div>
             </section>
         </>
