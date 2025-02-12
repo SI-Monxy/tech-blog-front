5aac4f7d8ee7b916e5fce844da29db2c217c0d1b
monxy747 <12smnms20@gmail.com>
2025-02-12 23:57:01 +0900

    ヘッダーのレイアウトを修正

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,3 +1,5 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 export default function MarketingLayout({
@@ -8,9 +10,9 @@ export default function MarketingLayout({
     return (
         <div>
             <header className="container z-40 bg-background">
-                <div className="h-20 py-6">
+                <div className="h-20 py-6 ml-6">
                     <nav>
-                        <Link href={"/login"}>ログイン</Link>
+                        <Link href={"/login"} className={cn(buttonVariants({ variant: "secondary", size: "default"}), "px-4")}>ログイン</Link>
                     </nav>
                 </div>
             </header>
