6794c64bc4755b265d9d434dcc636c2ee6503c46
monxy747 <12smnms20@gmail.com>
2025-02-13 23:24:55 +0900

    ブログ表示画面を作成

---
+++ app/(marketing)/blog/page.tsx
@@ -0,0 +1,39 @@
+import { allPosts } from "@/.contentlayer/generated";
+import Image from "next/image";
+
+export default function BlogPage() {
+
+    const posts = allPosts;
+
+    return (
+        <div className="container mx-auto p-4 max-w-4xl py-6 lg:py-10">
+            <div>
+                <div className="space-y-4">
+                    <h1 className="font-extrabold text-4xl lg:text-5xl tracking-tight">
+                        Blog 🚀
+                    </h1>
+                    <p className="text-muted-foreground text-xl">
+                        ContentLayerとMDXで書いています。
+                    </p>
+                </div>
+            </div>
+            <hr className="my-8" />
+            {posts.map((post) => (
+                <article key={post._id} className="flex flex-col items-center text-left">
+                    {post.image && <Image src={post.image} alt={post.title} width={604} height={252} className="rounded-md border bg-muted"/>}
+                    <h2 className="text-2xl font-extrabold">
+                        {post.title}
+                    </h2>
+                    {post.description && ( 
+                        <p className="text-muted-foreground">{post.description}</p>
+                    )}
+                    {post.date && (
+                        <p className="text-sm text-muted-foreground">
+                            {post.date}
+                        </p>
+                    )}
+                </article>
+            ))}
+        </div>
+    );
+}
