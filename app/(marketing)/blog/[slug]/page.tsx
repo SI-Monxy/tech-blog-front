0ca3a01822d59d084b48d8ce2a4dfcde3f80285b
monxy747 <12smnms20@gmail.com>
2025-02-14 22:31:59 +0900

    記事詳細ページのレイアウトを修正

--- app/(marketing)/blog/[slug]/page.tsx
+++ app/(marketing)/blog/[slug]/page.tsx
@@ -1,7 +1,54 @@
-export default function PostPage() {
+import { allPosts } from "@/.contentlayer/generated";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+async function getPostFromSlug(slug: string) {
+    const post = allPosts.find((post) => post.slugAsParams === slug);
+    return post;
+}
+
+export default async function PostPage({
+    params,
+}: { 
+    params: { slug: string };
+}) {
+    const slug = params.slug;
+    const post = await getPostFromSlug(slug);
+
+    if (!post) {
+        notFound();
+    }
+
     return (
-        <div>
-            <h1>Post Page</h1>
-        </div>
+        <article className="container mx-auto max-w-3xl py-6 lg:py-10">
+            <div>
+                {post.date && (
+                    <time>Published on {format(post.date, "yyyy/MM/dd")}</time>
+                )}
+                <h1 className="mt-2 font-extrabold text-4xl lg:text-5xl leading-tight">
+                    {post.title}
+                </h1>
+            </div>
+            {post.image && (
+                <Image 
+                    src={post.image} 
+                    alt={post.title} 
+                    width={720} 
+                    height={405} 
+                    className="my-8 rounded-md border bg-muted"
+                />
+            )}
+            <div>{post.body.html}</div>
+            <hr className="mt-12" />
+            <div className="py-6 text-center lg:py-10">
+                <Link href={"/blog"} className={cn(buttonVariants({variant: "secondary"}))}>
+                    記事の一覧に戻る
+                </Link>
+            </div>
+        </article>
     );
 }
--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -33,6 +33,10 @@ export const Post = defineDocumentType(() => ({
             type: "string",
             resolve: (doc) => `/${doc._raw.flattenedPath}`,
         },
+        slugAsParams: {
+            type: "string",
+            resolve: (doc) => doc._raw.flattenedPath.split("/").splice(1)[0],
+        },
     },
 }));
 
