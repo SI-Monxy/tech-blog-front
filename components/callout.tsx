6923daf94bcf237dba87e3b6d13ba67142a40527
monxy747 <12smnms20@gmail.com>
2025-02-19 23:04:30 +0900

    エラーを修正

--- components/callout.tsx
+++ components/callout.tsx
@@ -1,6 +1,6 @@
-export default function Callout() {
+export default function Callout({children}: {children: React.ReactNode}) {
     return (
-        <div>
+        <div className="my-6 flex items-center rounded-md border border-1-4 p-4">
             <div>Hello Callout</div>
         </div>
     )
