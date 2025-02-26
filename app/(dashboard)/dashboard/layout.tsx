736b5b47ed0fe09b58633b353a54de2c3eab120c
monxy747 <12smnms20@gmail.com>
2025-02-27 00:20:54 +0900

    ダッシュボードページにサイドナビゲーションを追加

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-// import DashboardNav from "@/components/dashboard-nav";
+import DashboardNav from "@/components/dashboard-nav";
 import MainNav from "@/components/main-nav";
 import SiteFooter from "@/components/site-footer";
 import { dashboardConfig } from "@/config/dashboard";
@@ -11,18 +11,18 @@ export default function DashboardLayout({
   return (
     <div className="flex min-h-screen flex-col space-y-6">
       <header className="sticky top-0 z-40 border-b bg-background">
-        <div className="container flex items-center justify-between py-4 h-16">
+        <div className="container mx-auto flex items-center justify-between py-4 h-16">
           <MainNav items={dashboardConfig.mainNav} />
         </div>
       </header>
-      {/* <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
+      <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
         <aside className="hidden md:flex w-[200px] flex-col">
           <DashboardNav items={dashboardConfig.sidebarNav} />
         </aside>
         <main className="flex flex-col w-full flex-1 overflow-hidden">
           {children}
         </main>
-      </div> */}
+      </div>
       <SiteFooter />
     </div>
   );
